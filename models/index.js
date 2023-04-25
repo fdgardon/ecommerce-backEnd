f0397bfb08f6aa6fcb3e5885b4a888ba4b686e40
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-04-24 17:49:44 -0700

    index model is functioned

--- models/index.js
+++ models/index.js
@@ -3,14 +3,40 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsToMany } = require('./Category');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    model: Category,
+    foreignKey: "category_id",
+    unique: false
+  }
+}),
 
 // Categories have many Products
+Category.hasMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: "category_id"
+  }
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: "product_id"
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: "Tag_id"
+  }
+});
 
 module.exports = {
   Product,
