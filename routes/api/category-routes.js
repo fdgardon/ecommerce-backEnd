35b3d9b4bd21d59e16a15a1484df5fe3f41c5969
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-04-27 17:51:53 -0700

    Create Category is fixed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,9 +46,8 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  Category.create({
-    category_name: req.body.category_name,
-  })
+  Category.create({ category_name: req.body.category_name })
+
     .then((category) => res.status(200).json(category))
     .catch((err) => res.status(400).json(err));
 });
